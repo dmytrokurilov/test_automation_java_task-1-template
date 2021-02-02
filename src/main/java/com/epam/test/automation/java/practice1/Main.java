77a73f6a5252d24fbf2abeba8733afb0296edc23
dmytrokurilov <dmytrokurilov@gmail.com>
2021-02-02 11:32:44 +0200

    Task Done

--- src/main/java/com/epam/test/automation/java/practice1/Main.java
+++ src/main/java/com/epam/test/automation/java/practice1/Main.java
@@ -2,24 +2,29 @@ package com.epam.test.automation.java.practice1;
 
 public class Main {
 
-    /**
-     * <summary>
-     * Implement code according to description of task 1.
-     * </summary>
-     */
-    public static int task1(int n) {
-        //TODO: Delete line below and write your own solution;
-        throw new UnsupportedOperationException();
+    public static int task1(int n)
+    {
+        int result;
+        if (n > 0)
+        {
+            result = n * n;
+        }
+        else
+        {
+            result = -n;
+        }
+        return result;
     }
 
-    /**
-     * <summary>
-     * Implement code according to description of task 2.
-     * </summary>
-     */
-    public static int task2(int n) {
-        //TODO: Delete line below and write your own solution;
-        throw new UnsupportedOperationException();
+    public static int task2(int n)
+    {
+        int a = n / 100;
+        int b = n /10 % 10;
+        int c = n% 10;
+        int max = Math.max(Math.max(a, b), c);
+        int min = Math.min(Math.min(a, b), c);
+        int middle = a + b + c - max - min;
+        return max *100 + middle * 10 + min;
     }
 
 }
